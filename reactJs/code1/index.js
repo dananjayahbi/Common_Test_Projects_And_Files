f69984b0d598202b790419b93d013229cb23b17b
dananjayahbi <dananjayahbi@gmail.com>
2022-12-21 05:06:37 +0530

    save for break

---
+++ reactJs/code1/index.js
@@ -0,0 +1,10 @@
+const myElement = (
+    <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+    </ul>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
