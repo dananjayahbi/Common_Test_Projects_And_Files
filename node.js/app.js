86b5fd9d796eb540a68b253865be1db143865017
dananjayahbi <dananjayahbi@gmail.com>
2022-12-27 06:17:24 +0530

    save for break

--- node.js/app.js
+++ node.js/app.js
@@ -1,9 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'isurudananjaya838@gmail.com',
+    pass: 'Indusara'
+  }
+});
+
+var mailOptions = {
+  from: 'isurudananjaya838@gmail.com',
+  to: 'dananjayahbi@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
